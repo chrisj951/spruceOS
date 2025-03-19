e91790de277707647ba80edd06989349591dbb30
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2025-03-18 20:43:18 -0500

    First pass at device-specific firmware updater:
    -changed from json breakage to bind mount to hide fw app
    -fw version check depends on device in use
    -runtime modified to check and handle fw and spruce update apps regardless of device

--- App/-FirmwareUpdate-/firmwareUpdate.sh
+++ App/-FirmwareUpdate-/firmwareUpdate.sh
@@ -2,8 +2,14 @@
 
 . /mnt/SDCARD/spruce/scripts/helperFunctions.sh
 
-CONFIG="/mnt/SDCARD/App/-FirmwareUpdate-/config.json"
-FW_FILE="/mnt/SDCARD/spruce/FIRMWARE_UPDATE/miyoo282_fw.img"
+SD_ROOT="/mnt/SDCARD"
+FW_DIR="/mnt/SDCARD/spruce/FIRMWARE_UPDATE"
+case "$PLATFORM" in
+	"A30" ) FW_FILE="miyoo282_fw.img" ;;
+	"Flip" ) FW_FILE="miyoo353_fw.img" ;;
+	"Brick" ) FW_FILE="trimui_tg3040.awimg" ;;
+	"SmartPro" ) FW_FILE="trimui_tg5040.awimg" ;;
+esac
 
 [ "$PLATFORM" = "SmartPro" ] && BG_IMAGE="/mnt/SDCARD/spruce/imgs/bg_tree_wide.png" || BG_IMAGE="/mnt/SDCARD/spruce/imgs/bg_tree.png"
 
@@ -12,7 +18,41 @@ FREE_SPACE="$(df -m /mnt/SDCARD | awk '{print $4}' | tail -n 1)"
 
 CHARGING="$(cat /sys/devices/platform/axp22_board/axp22-supplyer.20/power_supply/battery/online)"
 CAPACITY="$(cat /sys/devices/platform/axp22_board/axp22-supplyer.20/power_supply/battery/capacity)"
-VERSION="$(cat /usr/miyoo/version)"
+
+NEEDS_UPDATE=true
+
+case "$PLATFORM" in
+	"A30" )
+		VERSION="$(cat /usr/miyoo/version)"
+		if [ "$VERSION" -ge 20240713100458 ]; then
+			NEEDS_UPDATE=false
+		fi
+		;;
+	"Flip" )
+		VERSION="$(cat /usr/miyoo/version)"
+		if [ "$VERSION" -ge 20250228101926 ]; then
+			NEEDS_UPDATE=false
+		fi
+		;;
+	"Brick" )
+		VERSION="$(cat /etc/version)"
+		v_major="$(cut -d '.' -f 1 "$VERSION")"
+		# v_minor="$(cut -d '.' -f 2 "$VERSION")"
+		v_bug="$(cut -d '.' -f 3 "$VERSION")"
+		if [ "$v_major" -ge 1 ] && [ "$v_bug" -ge 6 ]; then
+			NEEDS_UPDATE=false
+		fi
+		;;
+	"SmartPro" )
+		VERSION="$(cat /etc/version)"
+		v_major="$(cut -d '.' -f 1 "$VERSION")"
+		# v_minor="$(cut -d '.' -f 2 "$VERSION")"
+		v_bug="$(cut -d '.' -f 3 "$VERSION")"
+		if [ "$v_major" -ge 1 ] && [ "$v_bug" -ge 4 ]; then
+			NEEDS_UPDATE=false
+		fi
+		;;
+esac
 
 # Debugging Variables
 SKIP_VERSION_CHECK=false
@@ -26,26 +66,25 @@ log_message "firmwareUpdate.sh: current FW version: $VERSION"
 cancel_update() {
 	log_message "firmwareUpdate.sh: Firmware update cancelled."
 	display -i "$BG_IMAGE" -o -t "Firmware update cancelled."
-	if [ -f "/mnt/SDCARD/miyoo282_fw.img" ]; then
-		rm "/mnt/SDCARD/miyoo282_fw.img"
+	if [ -f "$SD_ROOT/$FW_FILE" ]; then
+		rm "$SD_ROOT/$FW_FILE"
 		log_message "Removing FW image from root of card."
 	fi
 	exit 1
 }
 
 confirm_update() {
-	if [ ! -f "/mnt/SDCARD/miyoo282_fw.img" ]; then
+	if [ ! -f "$SD_ROOT/$FW_FILE" ]; then
 		display -i "$BG_IMAGE" -d 2 -t "Moving firmware update file into place."
-		cp "$FW_FILE" "/mnt/SDCARD/"
+		cp "$FW_DIR/$FW_FILE" "$SD_ROOT/"
 	fi
-	display -i "$BG_IMAGE" -t "Your A30 will now shut down. Please manually power your device back on while plugged in to complete the Miyoo firmware update. Once started, please be patient, as it will take a few minutes. It will power itself down again once complete." -p 140 -o
-	sed -i 's|"label":|"#label":|' "$CONFIG"
-	flag_add "first_boot_A30"
+	display -i "$BG_IMAGE" -t "Your $PLATFORM will now shut down. Please manually power your device back on while plugged in to complete the manufacturer firmware update. Once started, please be patient, as it will take a few minutes. It will power itself down again once complete." -p 140 -o
+	flag_add "first_boot_$PLATFORM"
 	sync
 	poweroff
 }
 
-if [ "$SKIP_VERSION_CHECK" = false ] && [ "$VERSION" -ge 20240713100458 ]; then
+if [ "$SKIP_VERSION_CHECK" = false ] && [ "$NEEDS_UPDATE" = false ]; then
 	log_message "firmwareUpdate.sh: Firmware already up to date. Hiding -FirmwareUpdate- App."
 	display -i "$BG_IMAGE" -o -t "Firmware is up to date - happy gaming!!!!!!!!!!"
 	sed -i 's|"label":|"#label":|' "$CONFIG"
@@ -54,7 +93,7 @@ else
 	log_message "firmwareUpdate.sh: Firmware requires update. Continuing."
 fi
 
-display -i "$BG_IMAGE" -t "A firmware update from Miyoo is ready for your device. We'll check your device's status and prepare the Miyoo update file. Once setup the Miyoo firmware updater will install it." -o -p 160
+display -i "$BG_IMAGE" -t "A firmware update from the manufacturer is ready for your device. We'll check your device's status and prepare the manufacturer update file. Once set up, the manufacturer firmware updater will install it." -o -p 160
 
 if [ "$FREE_SPACE" -lt 64 ]; then
 	log_message "firmwareUpdate.sh: Not enough free space on card. Aborting."
@@ -66,14 +105,14 @@ fi
 
 if [ "$CAPACITY" -lt 10 ]; then
 	log_message "firmwareUpdate.sh: Not enough charge on device. Aborting."
-	display -i "$BG_IMAGE" -o -t "As a precaution, please charge your A30 to at least 10% capacity, then try again."
+	display -i "$BG_IMAGE" -o -t "As a precaution, please charge your $PLATFORM to at least 10% capacity, then try again."
 	exit 1
 else
 	log_message "firmwareUpdate.sh: Device has at least 10% charge. Continuing."
 fi
 
 if [ "$CHARGING" -eq 0 ]; then
-	log_message "firmwareUpdate.sh: Device not plugged in. Prompting user to plug in their A30."
+	log_message "firmwareUpdate.sh: Device not plugged in. Prompting user to plug in their $PLATFORM."
 	while true; do
 		display -i "$BG_IMAGE" -t "Please connect your device to a power source to proceed with the update process." --confirm
 		if confirm; then
@@ -96,7 +135,7 @@ else
 fi
 
 if [ "$CHARGING" -eq 1 ]; then
-	log_message "firmwareUpdate.sh: Device is plugged in. Prompting for SELECT to proceed or B to cancel."
+	log_message "firmwareUpdate.sh: Device is plugged in. Prompting for A to proceed or B to cancel."
 	display -i "$BG_IMAGE" -t "WARNING: If unplugged or powered off before the update is complete, your device could become temporarily bricked, requiring you to run the unbricker software." --okay
 	if confirm; then
 		log_message "firmwareUpdate.sh: A button pressed. Confirming update."
@@ -110,6 +149,6 @@ if [ "$CHARGING" -eq 1 ]; then
 	fi
 
 else
-	display -d 3 -i "$BG_IMAGE" -t "Exiting Firmware Update app. Please try again once you have plugged in your A30."
+	display -d 3 -i "$BG_IMAGE" -t "Exiting Firmware Update app. Please try again once you have plugged in your $PLATFORM."
 	log_message "firmwareUpdate.sh: Device still not plugged in. Aborting."
 fi
